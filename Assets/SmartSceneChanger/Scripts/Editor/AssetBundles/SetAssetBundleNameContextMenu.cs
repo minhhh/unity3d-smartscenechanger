8e4261a8aabfa7db77627625b54534d726492d9b
ciitt <ciittwu.ktwyktw@gmail.com>
2017-06-05 16:03:37 +0900

    add : set AssetBundle name by right click menu

---
+++ Assets/SmartSceneChanger/Scripts/Editor/AssetBundles/SetAssetBundleNameContextMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace SSC
+{
+
+    public class SetAssetBundleNameContextMenu
+    {
+
+        [MenuItem("Assets/SSC/Set AssetBundle Name to File Path", true)]
+        private static bool ValidateSetAssetBundleNames()
+        {
+            return Selection.objects.Length > 0;
+        }
+
+        [MenuItem("Assets/SSC/Set AssetBundle Name to File Path")]
+        public static void SetAssetBundleNames()
+        {
+
+            if (!EditorUtility.DisplayDialog("Set AssetBundle Name to File Path", "Continue ?", "Yes", "Cancel"))
+            {
+                return;
+            }
+
+            // --------------
+
+            string path = "";
+
+            AssetImporter ai = null;
+
+            foreach (var obj in Selection.objects)
+            {
+
+                path = AssetDatabase.GetAssetPath(obj);
+
+                ai = AssetImporter.GetAtPath(path);
+
+                if(ai)
+                {
+
+                    path = path.Remove(0, 7).ToLower().Replace(' ', '_');
+                    path = Path.ChangeExtension(path, ".unity3d");
+
+                    ai.assetBundleName = path;
+                }
+
+            }
+
+            EditorUtility.DisplayDialog("Set AssetBundle Name to File Path", "Done.", "OK");
+
+        }
+
+    }
+
+}
