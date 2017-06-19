78560bf4434487a4db3768d84c8d661f441155fe
ciitt <ciittwu.ktwyktw@gmail.com>
2017-06-19 15:16:08 +0900

    add : bit mask attribute for enum

---
+++ Assets/SmartSceneChanger/Scripts/Editor/PropertyDrawer/BiMaskDrawer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace SSC
+{
+
+    [CustomPropertyDrawer(typeof(BiMaskAttribute))]
+    public class BiMaskDrawer : PropertyDrawer
+    {
+
+        /// <summary>
+        /// OnGUI
+        /// </summary>
+        /// <param name="position">Rect</param>
+        /// <param name="property">SerializedProperty</param>
+        /// <param name="label">GUIContent</param>
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            property.intValue = EditorGUI.MaskField(position, label, property.intValue, property.enumNames);
+        }
+
+    }
+
+}
