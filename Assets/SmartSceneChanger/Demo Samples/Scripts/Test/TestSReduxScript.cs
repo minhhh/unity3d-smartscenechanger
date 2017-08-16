40aa15dd031596a970f971dc924da5fbd1143bca
ciitt <ciittwu.ktwyktw@gmail.com>
2017-08-16 17:03:31 +0900

    add : add test code for SRedux

---
+++ Assets/SmartSceneChanger/Demo Samples/Scripts/Test/TestSReduxScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SSCSample
+{
+
+    public class TestSReduxScript : MonoBehaviour
+    {
+
+        void Start()
+        {
+            SSC.SimpleReduxManager.Instance.addSceneChangeStateReceiver(this.onSceneChangeState);
+            SSC.SimpleReduxManager.Instance.addPauseStateReceiver(this.onPauseStateState);
+        }
+
+        void onSceneChangeState(SSC.SceneChangeState scState)
+        {
+            print("onSceneChangeState : " + scState.stateEnum.ToString());
+        }
+
+        void onPauseStateState(SSC.PauseState pState)
+        {
+            print("onPauseStateState : " + pState.pause.ToString());
+        }
+
+    }
+
+}
