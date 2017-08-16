4fba8a7c734d4c04dd73672003871dd35f828219
ciitt <ciittwu.ktwyktw@gmail.com>
2017-08-16 17:07:24 +0900

    update : TestSReduxScript

--- Assets/SmartSceneChanger/Demo Samples/Scripts/Test/TestSReduxScript.cs
+++ Assets/SmartSceneChanger/Demo Samples/Scripts/Test/TestSReduxScript.cs
@@ -8,17 +8,32 @@ namespace SSCSample
     public class TestSReduxScript : MonoBehaviour
     {
 
+        // ----------------------------------------------------------------------------------------------------
         void Start()
         {
             SSC.SimpleReduxManager.Instance.addSceneChangeStateReceiver(this.onSceneChangeState);
             SSC.SimpleReduxManager.Instance.addPauseStateReceiver(this.onPauseStateState);
         }
 
+        // ----------------------------------------------------------------------------------------------------
+        void testFunc()
+        {
+
+            SSC.SceneChangeState scState = SSC.SimpleReduxManager.Instance.SceneChangeStateWatcher.state();
+            SSC.PauseState pState = SSC.SimpleReduxManager.Instance.PauseStateWatcher.state();
+
+            print("onSceneChangeState : " + scState.stateEnum.ToString());
+            print("onPauseStateState : " + pState.pause.ToString());
+
+        }
+
+        // ----------------------------------------------------------------------------------------------------
         void onSceneChangeState(SSC.SceneChangeState scState)
         {
             print("onSceneChangeState : " + scState.stateEnum.ToString());
         }
 
+        // ----------------------------------------------------------------------------------------------------
         void onPauseStateState(SSC.PauseState pState)
         {
             print("onPauseStateState : " + pState.pause.ToString());
