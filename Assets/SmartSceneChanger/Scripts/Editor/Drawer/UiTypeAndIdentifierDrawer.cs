d746fa74f453127ef04611eae3239fadaacfb708
ciitt <ciittwu.ktwyktw@gmail.com>
2017-08-16 16:54:47 +0900

    add : UiType

---
+++ Assets/SmartSceneChanger/Scripts/Editor/Drawer/UiTypeAndIdentifierDrawer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace SSC
+{
+
+    [CustomPropertyDrawer(typeof(UiTypeAndIdentifier))]
+    public class UiTypeAndIdentifierDrawer : PropertyDrawer
+    {
+
+        /// <summary>
+        /// OnGUI
+        /// </summary>
+        /// <param name="position">Rect</param>
+        /// <param name="property">SerializedProperty</param>
+        /// <param name="label">GUIContent</param>
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+
+            EditorGUI.BeginProperty(position, label, property);
+
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+            var leftRect = new Rect(position.xMin, position.y, position.width / 2, position.height);
+            var rightRect = new Rect(position.xMin + (position.width / 2), position.y, position.width / 2, position.height);
+
+            EditorGUI.PropertyField(leftRect, property.FindPropertyRelative("uiType"), GUIContent.none);
+            EditorGUI.PropertyField(rightRect, property.FindPropertyRelative("uiIdentifier"), GUIContent.none);
+
+            EditorGUI.EndProperty();
+
+        }
+
+    }
+
+}
---
+++ Assets/SmartSceneChanger/Scripts/Structs/UiType.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SSC
+{
+
+    public enum UiType
+    {
+        CommonUi,
+        SceneUi
+    }
+
+    [Serializable]
+    public class UiTypeAndIdentifier
+    {
+        public UiType uiType = UiType.CommonUi;
+        public string uiIdentifier = "";
+
+        public UiTypeAndIdentifier()
+        {
+
+        }
+
+        public UiTypeAndIdentifier(UiType _uiType, string _uiIdentifier)
+        {
+            this.uiType = _uiType;
+            this.uiIdentifier = _uiIdentifier;
+        }
+
+    }
+
+}
